90d96271456086ad8db2e3b6520a8fe24bcb81fd
Shikhin Sethi <draumr.shikhin@gmail.com>
2014-03-15 15:09:29 +0530

    Optimized (somewhat) rpncalc.

--- langs/brainfuck.asm
+++ langs/brainfuck.asm
@@ -7,34 +7,28 @@ org 0x7E00
 %macro debugger 0
 	; Current IP
 	mov ax, si
-	xchg al, ah
-	call hexprint8
-	xchg al, ah
-	call hexprint8
+	call hexprint16
 	mov al, ' '
 	call putchar
-	
+
 	; Current command
 	mov al, [si]
 	call putchar
 	mov al, ' '
 	call putchar
-	
+
 	; Current tape pointer
 	mov ax, bp
-	xchg al, ah
-	call hexprint8
-	xchg al, ah
-	call hexprint8
+	call hexprint16
 	mov al, ' '
 	call putchar
-	
+
 	; Current tabe symbol
 	mov al, [es:bp]
 	call hexprint8
 	mov al, 10
 	call putchar
-	
+
 	; Wait for keypress
 	call getch
 %endmacro
@@ -42,25 +36,25 @@ org 0x7E00
 start:
 	pusha
 	push es
-	
+
 	; Tape & tape pointer
 	mov ax, 0x1000
 	mov es, ax
 	xor bp, bp
-	
+
 	; Zero tape
 	xor di, di
 	mov cx, 0xFFFF
 	xor al, al
 	rep stosb
-	
+
 	; SI points to source, DI to end.
 	lea si, [bx + 2]
 	mov di, si
 	add di, [bx]
-	
+
 	call interpret
-	
+
 	pop es
 	popa
 	ret
@@ -68,90 +62,90 @@ start:
 interpret:
 	cmp si, di
 	je .end
-	
+
 	%ifdef enable_debugger
 		debugger
 	%endif
-	
+
 	lodsb
-	
+
 	.inc:
 		cmp al, '+'
 		jne .dec
-		
+
 		inc byte [es:bp]
-		
+
 		jmp interpret
 	.dec:
 		cmp al, '-'
 		jne .next
-		
+
 		dec byte [es:bp]
-		
+
 		jmp interpret
 	.next:
 		cmp al, '>'
 		jne .prev
-		
+
 		inc bp
-		
+
 		jmp interpret
 	.prev:
 		cmp al, '<'
 		jne .putchar
-		
+
 		dec bp
-		
+
 		jmp interpret
 	.putchar:
 		cmp al, '.'
 		jne .getchar
-		
+
 		mov al, [es:bp]
 		call putchar
-		
+
 		jmp interpret
 	.getchar:
 		cmp al, ','
 		jne .while
-		
+
 		call getch
 		call putchar
 		cmp al, 04
 		jne .not_eof
-		
+
 		.eof:
 			xor al, al
 		.not_eof:
 			mov [es:bp], al
-		
+
 		jmp interpret
 	.while:
 		cmp al, '['
 		jne .wend
-		
+
 		; Handle loop nesting by recursion inside a loop
 		.loop:
 			cmp byte [es:bp], 0
 			je .skip
-			
+
 			push si
 			call interpret
 			pop si
-			
+
 			jmp .loop
 		.skip:
 			; Skip after the loop(s). Needed because si is returned to same point where it left
 			mov cx, 1
-			
+
 			.skiploop:
 				lodsb
-				
+
 				cmp al, ']'
 				je .shallower
 				cmp al, '['
 				jne .skiploop
-				
+
 				.deeper:
 					inc cx
 					jmp .skiploop
@@ -163,7 +157,7 @@ interpret:
 	.wend:
 		cmp al, ']'
 		jne interpret
-		
+
 		; Fall through.
 	.end:
 		ret
--- langs/rpncalc.asm
+++ langs/rpncalc.asm
@@ -1,6 +1,7 @@
 %include "wodscipe.inc"
-	org 0x7E00
-	numinp_flag EQU 0x504
+org 0x7E00
+
+numinp_flag EQU 0x504
 
 %macro bppush 1
 	sub bp, 2
@@ -15,148 +16,143 @@
 main:
 	pusha
 	push es
-	
+
 	; Stack
 	xor bp, bp
 	mov ax, 0x1000
 	mov es, ax
-	
+
 	; Number input
 	xor dx, dx
-	
+
 	; Code
-	mov si, 0x8002
-	mov cx, [0x8000]
-	add cx, 0x8002
-	
+	lea si, [bx + 2]
+	mov cx, si
+	add cx, [bx]
+
 	.mainloop:
 		cmp si, cx
 		je end
-		
+
 		lodsb
-		
+
 		.num:
 			.num09:
 				cmp al, '0'
 				jb .notnum
 				cmp al, '9'
 				ja .numAF
-				
+
 				sub al, '0'
 				jmp .storenum
-				
+
 			.numAF:
 				cmp al, 'A'
 				jb .notnum
 				cmp al, 'F'
 				ja .numaf
-				
+
 				sub al, 'A'-10
 				jmp .storenum
-				
+
 			.numaf:
 				cmp al, 'a'
 				jb .notnum
 				cmp al, 'f'
 				ja .notnum
-				
+
 				sub al, 'a'-10
-				
+
 			.storenum:
 				shl dx, 4
 				xor ah, ah
 				add dx, ax
-				
+
 				mov byte [numinp_flag], 1
-				
+
 				jmp .mainloop
 		.notnum:
 			cmp byte [numinp_flag], 0
 			je .print
-			
+
 			bppush dx
-			
+
 			; Unset number input flag and clear dx for next use
 			mov byte [numinp_flag], 0
 			xor dx, dx
-			
+
 		.print:
 			cmp al, 'p'
 			jne .add
-			
+
 			bppop ax
-			
+
 			; High byte
-			xchg al, ah
-			call hexprint8
-			
-			; Low byte
-			xchg al, ah
-			call hexprint8
-			
+			call hexprint16
+
 			; Newline
 			mov al, 10
 			call putchar
-			
+
 		.add:
 			cmp al, '+'
 			jne .sub
-			
+
 			bppop bx
 			bppop ax
-			
+
 			add ax, bx
-			
+
 			bppush ax
-			
+
 			jmp .mainloop
-		
+
 		.sub:
 			cmp al, '-'
 			jne .mul
-			
+
 			bppop bx
 			bppop ax
-			
+
 			sub ax, bx
-			
+
 			bppush ax
-			
+
 			jmp .mainloop
-			
+
 		.mul:
 			cmp al, '*'
 			jne .div
-			
+
 			bppop bx
 			bppop ax
-			
+
 			mul bx
-			
+
 			bppush ax
 			xor dx, dx ; Zero dx for number input
-			
+
 			jmp .mainloop
-		
+
 		.div:
 			cmp al, '/'
 			jne .mainloop
-			
+
 			bppop bx
 			bppop ax
-			
+
 			xor dx, dx
 			div bx
-			
+
 			bppush ax
 			xor dx, dx ; Zero dx for number input
-			
+
 			jmp .mainloop
-			
+
 end:
 	mov si, endmsg
 	call puts
-	
+
 	pop es
 	popa
 	ret
